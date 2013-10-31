002bd9dc138c59ee0701dd3ee00c4b5a6b98e324
Michael Schleichardt <michael@schleichardt.net>
2013-10-31 08:40:45 +0100

    Use JUnit runner to retrieve system properties.

--- test/LargeScalaTest.scala
+++ test/LargeScalaTest.scala
@@ -1,5 +1,8 @@
+import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class LargeScalaTest extends Specification {
 
   val props = System.getProperties()
